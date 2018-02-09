d8af20aca6d7778a5db840a367ed58129296698f
amrkoow <36312213+amrkoow@users.noreply.github.com>
2018-02-09 23:30:55 +0100

    Add files via upload
    the basic python script featuring aspect ratio, different output window sizes and boundary conditions for the mandelbrot set calculation.
    All inputs are hard coded.

---
+++ mandelbrot.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from tkinter import *
+import matplotlib
+
+# =============================================================================
+# Startvorgaben --> sollen später in GUI
+# =============================================================================
+
+imgwidth=400
+aspectratio=1
+imgheight=int(imgwidth/aspectratio)
+
+lefto=-2.25
+righto= 0.75
+horcenter=(lefto+righto)/2
+
+topo=1.5
+bottomo=-1.5
+vertcenter=(bottomo+topo)/2
+
+# Mandelbrotmegenparameter
+maxiter=60      #Anzahl der maximalen Iterationsschritte
+limit=2         #Absolutwertlimit, über dem die Iteration abgebrochen wird
+
+#Colormap
+cmpa=matplotlib.cm.get_cmap('gist_ncar')
+
+# =============================================================================
+# Berechnung Bildparameter
+# =============================================================================
+
+left= horcenter-(((topo-bottomo)/2)*aspectratio)
+right= horcenter+(((topo-bottomo)/2)*aspectratio)
+
+top= vertcenter-((topo-bottomo)/2)
+bottom= vertcenter+((topo-bottomo)/2)
+
+tk=Tk()
+tk.title("Mandelbrot")
+
+canvas=Canvas(tk, width=imgwidth, height=imgheight)
+
+# =============================================================================
+# Mandelbrot Iteration
+# =============================================================================
+
+for x in range (imgwidth):
+    cr=left+x*(right-left)/imgwidth
+    for y in range (imgheight):
+        ci=bottom+y*(top-bottom)/imgheight
+        c=complex(cr, ci)
+        z=0
+        i=0
+        for i in range(maxiter):
+            if abs(z)>limit:
+                break
+            z=(z**2)+c
+            
+            # Zeichnen
+            
+            mycolor1=cmpa(i/maxiter)
+            mycolor2=mycolor1[:3]
+            mycolor=matplotlib.colors.rgb2hex(mycolor2)
+            if i == (maxiter -1):
+                mycolor='#%02x%02x%02x' % (0,0,0)
+            canvas.create_line(x,y, x+1, y+1, fill= mycolor)
+            
+# =============================================================================
+# Bildschirmausgabe
+# =============================================================================
+
+canvas.pack()
+
+canvas.mainloop()
